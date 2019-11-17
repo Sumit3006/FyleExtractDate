833ba4d38a68797a80f73426f65be773a1fc6058
sumit <sumitku1404@gmail.com>
2019-11-17 23:19:34 +0530

    Final improvements with accuracy 0.40

--- regex_date.py
+++ regex_date.py
@@ -23,7 +23,8 @@ def match_type_1(input_string):
             if not a.isdigit():
                 x=a
                 break
-
+        if s.count(x)<2:
+            return
         l=s.split(r'{}'.format(x))
         print(l)
         mon=int(l[1])
@@ -49,7 +50,8 @@ def match_type_7(input_string):
             if not a.isdigit():
                 x=a
                 break
-
+        if(s.count(x)<2):
+            return 
         l=s.split(r'{}'.format(x))
         mon=int(l[1])
         year=int(l[2])
@@ -65,20 +67,19 @@ def match_type_7(input_string):
             p[2]=l[1]
             return "-".join(p)
         else:
-            l=s.split(r'/')
             if year>19:
                 year=19*100+year
             else:
                 year=20*100+year    
             p=[1,2,3]
             p[0]=str(year)
-            p[1]=l[1]
+            p[1]=str(mon)
             p[2]=l[0]
             return "-".join(p) 
 
 
 def match_type_7a(input_string):
-    m = re.findall(r"[\d]{1,2}-[\d]{1,2}-[\d]{2}\s", input_string)
+    m = re.findall(r"[\d]{1,2}[-][\d]{1,2}[-][\d]{2}\s", input_string)
     for s in m:
         s=s.strip()
         l=s.split(r'-')
@@ -109,7 +110,7 @@ def match_type_7a(input_string):
             
 #07-May-2018
 def match_type_8(input_string):
-    all =re.findall(r'\s\d\d[- /.]\w\w\w[- /.]\d\d\d\d\s', input_string)
+    all =re.findall(r'\s[\d]{1,2}[- /.]\w\w\w[- /.]\d\d\d\d\s', input_string)
     for s in all:
         s=s.strip()
         print("A"+s+"a")
@@ -118,7 +119,8 @@ def match_type_8(input_string):
             if not a.isdigit():
                 x=a
                 break
-
+        if s.count(x)<2:
+            return
         l=s.split(r'{}'.format(x))
     
         mon=l[1]
@@ -156,19 +158,21 @@ def match_type_8(input_string):
         return "-".join(p)
 #07-mAY-18
 def match_type_8a(input_string):
-    all =re.findall(r'\s\d\d[- /.]\w\w\w[- /.]\d\d\s', input_string)
+    all =re.findall(r'\s[\d]{1,2}[- /.][A-Za-z]{3}[- /.]\d\d\s', input_string)
     for s in all:
+        print(s)
         s=s.strip()
         x='-'
         for a in s:
             if not a.isdigit():
                 x=a
                 break
-
+        if s.count(x)<2:
+            return
         l=s.split(r'{}'.format(x))
         mon=l[1]
         act_mon="00"
-        year=l[2]
+        year=int(l[2])
         if year>19:
                 year=19*100+year
         else:
@@ -207,19 +211,20 @@ def match_type_8a(input_string):
         return "-".join(p)       
                 
 def match_type_9(input_string):
-    all =re.findall(r'\s\w\w\w[- /.]\d\d[-]\d\d\s', input_string)
+    all =re.findall(r'[A-Za-z]{3}[- /.]{0,1}[\d]{1,2}[\w]{0,1}\d\d\s', input_string)
     for s in all:
         s=s.strip()
-        x='-'
-        for a in s:
-            if not a.isdigit():
-                x=a
-                break
+        mon=s[:3]
+        day=" "
+        if s[3].isdigit():
+            day=s[3:5]
+        else:
+            day=s[4:6]
+        if not day[1].isdigit():
+            day=day[:1]
+        year=int(s[-2:])
 
-        l=s.split(r'{}'.format(x))
-        mon=l[0]
         act_mon="00"
-        year=l[2]
         if year>19:
                 year=19*100+year
         else:
@@ -254,10 +259,10 @@ def match_type_9(input_string):
         p=[1,2,3]
         p[0]=year
         p[1]=act_mon
-        p[2]=l[0]
+        p[2]=day
         return "-".join(p)     
 def match_type_10(input_string):
-    all =re.findall(r'\s\w\w\w[- /.]\d\d[- /.]\d\d\d\d\s', input_string)
+    all =re.findall(r'\s\w\w\w[- /.][\d]{1,2}[- /.]\d\d\d\d\s', input_string)
     for s in all:
         s=s.strip()
         x='-'
@@ -265,7 +270,8 @@ def match_type_10(input_string):
             if not a.isdigit():
                 x=a
                 break
-
+        if s.count(x)<2:
+            return
         l=s.split(r'{}'.format(x))
         mon=l[0]
         act_mon="00"
@@ -304,7 +310,7 @@ def match_type_10(input_string):
 
 
 def match_type_11(input_string):
-    all =re.findall(r'\s\d\d[- /.]\w\w\w[- /.]\d\d\d\d\s', input_string)
+    all =re.findall(r'\s[\d]{1,2}[- /.]\w\w\w[- /.]\d\d\d\d\s', input_string)
     for s in all:
         x='-'
         s=s.strip()
@@ -312,7 +318,8 @@ def match_type_11(input_string):
             if not a.isdigit():
                 x=a
                 break
-
+        if s.count(x)<2:
+            return
         l=s.split(r'{}'.format(x))
         mon=l[1]
         act_mon="00"
@@ -357,7 +364,8 @@ def match_type_12(input_string):
             if not a.isdigit():
                 x=a
                 break
-
+        if s.count(x)<2:
+            return
         l=s.split(r'{}'.format(x))
         mon=int(l[1])
         if(mon>12):
--- request_send.py
+++ request_send.py
@@ -1,6 +1,15 @@
 import requests
 import os
+import time
 
-for filename in os.listdir('Test_Photos'):
-    resp = requests.post("http://localhost:5000/predict",
-                        files={"file": open('Test_Photos' + '/' + filename,'rb')})
+cnt=0
+for filename in os.listdir('Original_Photos'):
+    resp = requests.post("http://127.0.0.1:5000/extract_date",
+                        files={"file": open('Original_Photos' + '/' + filename,'rb')})
+    time.sleep(1)
+    print(filename+" : "+resp.text)
+    if resp.text[-1:].isdigit():
+        cnt+=1
+ac=cnt/595
+with open("Accuracy.txt", "w") as text_file:
+        text_file.write("{0}".format(ac))
